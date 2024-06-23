d555737695600d8929fc7136d3d55f2a39bef9e3
agar <antoningaranto@protonmail.com>
2024-06-23 19:11:42 +0200

    init

---
+++ AvaloniaSample/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace AvaloniaSample.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ AvaloniaSample/Welcome/WelcomeUserControl.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaSample.Welcome;
+
+public partial class WelcomeUserControl : UserControl
+{
+    public WelcomeUserControl()
+    {
+        InitializeComponent();
+    }
+}
